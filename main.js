ecdb3f36fbecc59e67444142d37412d237dcc453
Thong <xuanthong.ttn@gmail.com>
2025-02-07 20:24:31 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+// alert (hien cua so thong báo)
+// console.log(fullName);
+// confirm('Xac nhan du tuoi') 
+// hoi xac minh
+// prompt = confirm+input
+// setTimeout(function(){
+//     alert('thongbao')
+// }, 1000)
+
+// setInterval(function(){
+//     console.log('Hello'+ Math.random())
+// },1)
+
+alert('Hi 222222');
+
+
